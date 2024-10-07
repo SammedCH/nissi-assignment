c4d79b366560640b7d5cf23587c97a5c591f693f
SammedCH <sammedhuchchannavar1308@gmail.com>
2024-10-07 16:14:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("deals").addEventListener("click", function() {
+    window.open("https://www.amazon.com/gp/goldbox", "_blank");
+});
+
+document.getElementById("customer-service").addEventListener("click", function() {
+    window.open("https://www.amazon.com/gp/help/customer", "_blank");
+});
+
+document.getElementById("registry").addEventListener("click", function() {
+    window.open("https://www.amazon.com/wedding", "_blank");
+});
+
+document.getElementById("gift-cards").addEventListener("click", function() {
+    window.open("https://www.amazon.com/gift-cards", "_blank");
+});
+
+document.getElementById("sell").addEventListener("click", function() {
+    window.open("https://www.amazon.com/sell", "_blank");
+});
